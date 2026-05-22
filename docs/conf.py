bd112d6ef8dd0de4c779d625d29eb6813a67e6b8
tellabot <andrey@tellabot.com>
2026-05-23 00:25:33 +0500

    Add files via upload

---
+++ docs/conf.py
@@ -0,0 +1,16 @@
+import os
+import sys
+
+project = 'Tell A Bot API Reference'
+copyright = '2025, Tell A Bot'
+author = 'Tell A Bot'
+release = '1.0'
+
+extensions = [
+    'myst_parser',
+]
+
+master_doc = 'index'
+exclude_patterns = ['_build']
+
+html_theme = 'sphinx_rtd_theme'
